0247e3abe4174309279dfdbb37581f09ee20b789
shiqianghui <squirrelzt@126.com>
2019-06-17 16:34:23 +0800

    登录布局修改

--- src/entry/index.js
+++ src/entry/index.js
@@ -1,9 +1,7 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Breadcrumb } from 'antd';
-import Nav from './../component/nav/nav.js';
-import Ftp from './../component/ftp/ftp.js';
 import Login from './../component/login/login.js';
 import Home from './../component/home/home.js';
 import User from './../component/user/user.js';
@@ -14,43 +12,33 @@ import './css/index.css';
 
 function component() {
   var element = document.createElement('pre');
-  element.innerHTML = '<div id="root">one</div>';
+  element.innerHTML = '<div id="root"></div>';
   return element;
 }
 
 let element = component(); 
 document.body.appendChild(element);
 
-const userSubCategory = ({ match }) => (
-  <div className="content-container">
-    <Route path="/home/user" component = { User }/>
-  </div>
-);
-
-const userDetailCategory = ({ match }) => (
-  <div className="content-container">
-    <Route path="/home/detail" component = { Detail }/>
-  </div>
-);
 
 ReactDOM.render((
   <Router>
     <div>
-      <Route path="/nav" component = { Nav } />
-      <Route path="/ftp" component = { Ftp } />
+      <Switch>
       <Route path='/login' component = { Login }/>
       <Route path='/home' component = { Home } />
-      <div className="content-container">
-        <div className="breadcrumb">
-            <Breadcrumb>
-                <Breadcrumb.Item key="user-manage">用户管理</Breadcrumb.Item>
-                <Breadcrumb.Item key="user-list">用户列表</Breadcrumb.Item>
-            </Breadcrumb>
+        <div className="content-container">
+          <div className="breadcrumb">
+              <Breadcrumb>
+                  <Breadcrumb.Item key="user-manage">用户管理</Breadcrumb.Item>
+                  <Breadcrumb.Item key="user-list">用户列表</Breadcrumb.Item>
+              </Breadcrumb>
+          </div>
+          <Route path='/home/user' component = { User }/>
+          <Route path='/home/detail' component = { Detail }/>
+          <Route path='/home/jobsource' component = { JobSource }/>
         </div>
-        <Route path='/home/user' component = { User }/>
-        <Route path='/home/detail' component = { Detail }/>
-        <Route path='/home/jobsource' component = { JobSource }/>
-      </div>
+      </Switch>
+     
     </div>
   </Router>
 ),document.getElementById('root')
