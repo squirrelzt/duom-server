b74ccd1aaec0c571d4631f7d9981c0189a53f0ca
shiqianghui <squirrelzt@126.com>
2019-07-01 13:26:31 +0800

    splitChunks

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -4,6 +4,30 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
+	optimization: {
+		splitChunks: {
+		  chunks: 'async',
+		  minSize: 30000,
+		  maxSize: 0,
+		  minChunks: 1,
+		  maxAsyncRequests: 5,
+		  maxInitialRequests: 3,
+		  automaticNameDelimiter: '~',
+		  automaticNameMaxLength: 30,
+		  name: true,
+		  cacheGroups: {
+			vendors: {
+			  test: /[\\/]node_modules[\\/]/,
+			  priority: -10
+			},
+			default: {
+			  minChunks: 2,
+			  priority: -20,
+			  reuseExistingChunk: true
+			}
+		  }
+		}
+	  },
     entry: {
         index: './src/entry/index.js'
 	},
